dbcaf7d9ffe8c74da6fd554db0e9e47a6bce3399
HazemAhmedSaad <zozahmed939@gmail.com>
2024-09-17 15:48:39 +0300

    create components and services

---
+++ src/app/components/serieses/serieses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-serieses',
+  standalone: true,
+  imports: [],
+  templateUrl: './serieses.component.html',
+  styleUrl: './serieses.component.css'
+})
+export class SeriesesComponent {
+
+}
