77d5b86369d36a69f2c45aa3be4fa6e0079aa717
HazemAhmedSaad <zozahmed939@gmail.com>
2024-09-21 19:46:20 +0300

    add series page

--- src/app/components/serieses/serieses.component.ts
+++ src/app/components/serieses/serieses.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { SeriesesService } from '../../services/serieses.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-serieses',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './serieses.component.html',
   styleUrl: './serieses.component.css'
 })
 export class SeriesesComponent {
+  seriesList: any[] = [];
+  constructor(private seriesesService: SeriesesService) { }
 
+  ngOnInit() {
+    this.seriesesService.getAllSeries().subscribe((seriesData: any) => {
+      this.seriesList = seriesData.results;
+      console.log(seriesData);
+    });
+  }
 }
--- src/app/services/serieses.service.ts
+++ src/app/services/serieses.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeriesesService {
-
-  constructor() { }
+  apiKey: string = "03861000422926c11be27fbb2aea0030";
+  constructor(private http: HttpClient) { }
+  getAllSeries() {
+    return this.http.get(`https://api.themoviedb.org/3/tv/popular?api_key=${this.apiKey}&language=en-US&page=1`);
+  }
 }
