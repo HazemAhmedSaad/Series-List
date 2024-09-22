1f0e250db773272b2cbb769253b028138e570a14
HazemAhmedSaad <zozahmed939@gmail.com>
2024-09-22 03:47:30 +0300

    add series details

--- src/app/components/serieses/serieses.component.ts
+++ src/app/components/serieses/serieses.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SeriesesService } from '../../services/serieses.service';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-serieses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './serieses.component.html',
   styleUrl: './serieses.component.css'
 })
--- src/app/services/serieses.service.ts
+++ src/app/services/serieses.service.ts
@@ -11,4 +11,8 @@ export class SeriesesService {
   getAllSeries() {
     return this.http.get(`https://api.themoviedb.org/3/tv/popular?api_key=${this.apiKey}&language=en-US&page=1`);
   }
+
+  getSeriesDetails(id: number) {
+    return this.http.get(`https://api.themoviedb.org/3/tv/${id}?api_key=${this.apiKey}&language=en-US`);
+  }
 }
